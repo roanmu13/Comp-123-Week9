ec6591f09a7208a32459956ed70f61b923e50770
Rosa <munguiarosa13@gmail.com>
2017-07-11 17:47:26 -0400

    Added initialize method to superHuman class

---
+++ Week9/Power.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+/*Rosa Munguia
+ Date: July 11 2017
+ Description: Created power class
+ Version: 0.1- Created for power class
+ */
+namespace Week9
+{/// <summary>
+/// this is the power class which will be used as a data type
+/// </summary>
+    public class Power
+    {
+        
+        //public properties
+        public string Name { get; set; }
+        public int Rank { get; set; }
+        //constructors
+
+        public SuperHuman(string name) :base(name)
+        {
+
+        }
+     
+    }
+}
--- Week9/SuperHuman.cs
+++ Week9/SuperHuman.cs
@@ -5,7 +5,7 @@ using System.Text;
 /*Rosa Munguia
  Date: July 11 2017
  Description: Super Human class that extends abstract human class
- Version: 0.1- Created SuperHuman class
+ Version: 0.2- Added _initialize method
  */
 namespace Week9
 {
@@ -15,17 +15,27 @@ namespace Week9
     public class SuperHuman : Human
     {
         //Private instance variable(fields)
-
+        private List<Power> _powers;
 
         //public properties
 
         //constructors
-
+        /// <summary>
+        /// main constructor for super human class. It takes one argument: name(string)
+        /// </summary>
+        /// <param name="name"></param>
         public SuperHuman(string name) :base(name)
         {
-
+            this._initialize();
         }
         //private methods
+        /// <summary>
+        /// this method initializaes, instantiates and assigns values to class properties
+        /// </summary>
+        private void _initialize()
+        {
+            this._powers = new List<Power>();//creates an empty list
+        }
 
         //public methods
     }
