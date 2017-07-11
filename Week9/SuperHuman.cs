0a5a602a12154f100d428207d791582df6bc238b
Rosa <munguiarosa13@gmail.com>
2017-07-11 17:35:47 -0400

    Added superHuman class

---
+++ Week9/SuperHuman.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+/*Rosa Munguia
+ Date: July 11 2017
+ Description: Super Human class that extends abstract human class
+ Version: 0.1- Created SuperHuman class
+ */
+namespace Week9
+{
+    /// <summary>
+    /// This is superHuman class
+    /// </summary>
+    public class SuperHuman : Human
+    {
+        //Private instance variable(fields)
+
+
+        //public properties
+
+        //constructors
+
+        public SuperHuman(string name) :base(name)
+        {
+
+        }
+        //private methods
+
+        //public methods
+    }
+}
