bf8fdf073626a45148a2825eb5f9459a2bae5c5e
Rosa <munguiarosa13@gmail.com>
2017-07-11 17:13:11 -0400

    Create new repository for lesson 9

--- Week9/Program.cs
+++ Week9/Program.cs
@@ -3,7 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+/*Rosa Munguia
+ Date: July 11 2017
+ Description: Demo for Lesson 9
+ Version: 0.1- Created for program class
+ */
 namespace Week9
 {
     class Program
