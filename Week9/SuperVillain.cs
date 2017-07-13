a49520f856bbd65d9dd67a9704c90773f5e3a3c4
Rosa <munguiarosa13@gmail.com>
2017-07-13 12:01:57 -0400

    Added IHasMalice interface

---
+++ Week9/SuperVillain.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+/*Rosa Munguia
+ Date: July 13 2017
+ Description: Creating superVillain Class
+ Version: 0.1- 
+ */
+namespace Week9
+    public class SuperVillain:SuperHuman
+    {
+    }
+}
