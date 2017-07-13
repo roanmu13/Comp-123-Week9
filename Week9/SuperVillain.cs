0b2dd27ab0f4dc65aedc1c55ffe11baf87124e13
Rosa <munguiarosa13@gmail.com>
2017-07-13 12:07:28 -0400

    Added superHero child class

--- Week9/SuperVillain.cs
+++ Week9/SuperVillain.cs
@@ -5,10 +5,33 @@ using System.Text;
 /*Rosa Munguia
  Date: July 13 2017
  Description: Creating superVillain Class
- Version: 0.1- 
+ Version: 0.1- Added superHero child class
  */
-namespace Week9
-    public class SuperVillain:SuperHuman
+namespace Week9 { 
+    /// <summary>
+    /// This is supervillain class
+    /// </summary>
+    public class SuperVillain:SuperHuman, IHasMalice
     {
+        //Private instance variables
+        private int _malice;
+        //Public Properties
+        public int Malice
+        {
+            get
+            {
+                return this._malice;
+            }
+            set
+            {
+                this._malice = value;
+            }
+        }
+        //Constructors
+        public SuperVillain(string name, int malice)
+            :base (name)
+        {
+            this._malice = malice;
+        }
     }
 }
